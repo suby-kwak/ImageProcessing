edc58768b114cbc5dd975775bdcfe85352299760
suby-kwak <rhkdbtj@gmail.com>
2023-03-28 17:41:50 +0900

    커밋 2

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import cv2
 
 # 영상 소스 열기
-src = cv2.imread('image/응시화면.jpg')
+src = cv2.imread('image/KakaoTalk_20221014_195404899.png')
 
+
+## 영상 처리 알고리즘 구현##
+dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
 
 
 
 # 영상 디스플레이
 cv2.imshow('src',src)
+cv2.imshow('dst1',dst1)
 
 
 # 마무리
 cv2.waitKey(0)
-cv2.destoryAllWindows()
+cv2.destroyAllWindows()
